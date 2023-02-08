f1c8dd13f34d99646ba96f77e5d0826c048e36a6
Akos Arva <arva23@gmail.com>
2023-02-08 20:01:41 +0100

    Position pair type

---
+++ src/main/java/com/mycompany/chessmotor/Pair.java
@@ -0,0 +1,20 @@
+package com.mycompany.chessmotor;
+
+// position pair type for squares
+public class Pair {
+
+    public int file;
+    public int rank;
+    
+    public Pair(){
+    
+        file = 0;
+        rank = 0;
+    }
+    
+    public Pair(int file, int rank){
+    
+        this.file = file;
+        this.rank = rank;
+    }
+}
