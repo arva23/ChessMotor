bdaabb43ee63d972a5912bab3d610c54412c70dd
Akos Arva <arva23@gmail.com>
2023-04-07 23:05:36 +0200

    switched file-rank order for coordinate order correction

--- src/main/java/chessmotor/enginecontroller/Step.java
+++ src/main/java/chessmotor/enginecontroller/Step.java
@@ -3,8 +3,8 @@ package chessmotor.enginecontroller;
 public class Step{
 
     private int pieceId;
-    private int rank;
     private int file;
+    private int rank;
     private double value;
     // counting changes in tendency for certain number of steps
     private int cumulativeChangeCount;
@@ -13,25 +13,25 @@ public class Step{
     public Step(){
 
         pieceId = -1;
-        rank = -1;
         file = -1;
+        rank = -1;
     }
 
-    public Step(int pieceId, int rank, int file, double value, 
+    public Step(int pieceId, int file, int rank, double value, 
             int cumulativeChangeCount, double cumulativeValue) throws Exception{
 
         this.pieceId = pieceId;
 
-        if(rank < 0 && 7 < rank)
-            throw new Exception("Rank is out of range.");
-
-        this.rank = rank;
-
         if(file < 0 || 7 < file)
             throw new Exception("File is out of range.");
 
-        this.file = file;
+        if(rank < 0 && 7 < rank)
+            throw new Exception("Rank is out of range.");
 
+        this.file = file;
+        
+        this.rank = rank;
+        
         this.cumulativeChangeCount = cumulativeChangeCount;
 
         this.cumulativeValue = cumulativeValue;
