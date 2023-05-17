96c6f6d10418118fdb54d114234f2638b670f2e2
Akos Arva <arva23@gmail.com>
2023-05-17 11:09:43 +0200

    Encapsulation fix of Pair

--- src/main/java/chessmotor/enginecontroller/Pair.java
+++ src/main/java/chessmotor/enginecontroller/Pair.java
@@ -3,8 +3,8 @@ package chessmotor.enginecontroller;
 // position pair type for squares
 public class Pair {
 
-    public int rank;
-    public int file;
+    private int rank;
+    private int file;
     
     /**
      * Default constructor of Pair class with initial values of 0
@@ -25,5 +25,54 @@ public class Pair {
     
         this.rank = rank;
         this.file = file;
+        
+    }
+    
+    /**
+     * Obtains rank
+     * @return Value of rank
+     */
+    public int getRank(){
+    
+        return rank;
+    }
+    
+    /**
+     * Obtains file
+     * @return Value of file
+     */
+    public int getFile(){
+    
+        return file;
+    }
+    
+    /**
+     * Sets new value for rank
+     * @param newRank New value for rank
+     * @throws ValueOutOfRangeException 
+     */
+    public void setRank(int newRank) throws ValueOutOfRangeException{
+    
+        if(newRank < 0){
+        
+            throw new ValueOutOfRangeException("Value underflow of rank.");
+        }
+        
+        this.rank = newRank;
+    }
+    
+    /**
+     * Sets new value for file
+     * @param newFile New value of file
+     * @throws ValueOutOfRangeException 
+     */
+    public void setFile(int newFile) throws ValueOutOfRangeException{
+    
+        if(newFile < 0){
+        
+            throw new ValueOutOfRangeException("Value underflow of file.");
+        }
+        
+        this.file = newFile;
     }
 }
